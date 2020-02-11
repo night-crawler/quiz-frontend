998957592db59be17553b372f5fad418d8372146
night-crawler <lilo.panic@gmail.com>
2020-02-11 15:41:05 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    kotlin("js") version "1.3.61"
+}
+
+group = "fm.force"
+version = "1.0-SNAPSHOT"
+
+val kotlinVersion: String by extra
+val reactVersion: String by extra
+val extensionsVersion: String by extra
+val reactRouterDomVersion: String by extra
+val reactReduxVersion: String by extra
+val reduxVersion: String by extra
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://kotlin.bintray.com/kotlin-js-wrappers/") }
+    maven { url = uri("https://dl.bintray.com/kotlin/kotlinx.html/") }
+}
+dependencies {
+    implementation(kotlin("stdlib-js"))
+
+    implementation("org.jetbrains:kotlin-extensions:$extensionsVersion-kotlin-$kotlinVersion")
+
+    implementation("org.jetbrains:kotlin-react:$reactVersion-kotlin-$kotlinVersion")
+    implementation("org.jetbrains:kotlin-react-dom:$reactVersion-kotlin-$kotlinVersion")
+
+    implementation("org.jetbrains:kotlin-react-router-dom:$reactRouterDomVersion-kotlin-$kotlinVersion")
+
+    implementation("org.jetbrains:kotlin-redux:$reduxVersion-kotlin-$kotlinVersion")
+    implementation("org.jetbrains:kotlin-react-redux:$reactReduxVersion-kotlin-$kotlinVersion")
+
+    testImplementation(kotlin("test-js"))
+}
+
+kotlin {
+    target {
+        browser()
+    }
+
+    sourceSets["main"].dependencies {
+        implementation(npm("react", reactVersion.split("-").first()))
+        implementation(npm("react-dom", reactVersion.split("-").first()))
+
+        implementation(npm("redux", reduxVersion.split("-").first()))
+        implementation(npm("react-redux", reactReduxVersion.split("-").first()))
+
+        implementation(npm("react-router-dom", reactRouterDomVersion.split("-").first()))
+
+        implementation(npm("core-js", "3.6.4"))
+    }
+}
